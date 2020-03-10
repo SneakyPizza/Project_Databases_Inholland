dd2cebe56d0ee10a3e30d4c77355107ed466131e
larsdeg <54888533+larsdeg@users.noreply.github.com>
2020-03-10 13:17:18 +0100

    Create Teacher_DAO.cs

---
+++ Someren1920F/SomerenDAL/Teacher_DAO.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+using System.Collections.ObjectModel;
+using SomerenModel;
+
+namespace SomerenDAL
+{
+    public class Teacher_DAO : Base
+    {
+        private SqlConnection dbConnection;
+
+        public Teacher_DAO()
+        {
+            string connString = ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString;
+            dbConnection = new SqlConnection(connString);
+        }
+
+        public List<Teacher> Db_Get_All_Teachers()
+        {
+            dbConnection.Open();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Person WHERE Role = 1", dbConnection);
+            SqlDataReader reader = cmd.ExecuteReader();
+            List<Teacher> teachers = new List<Teacher>();
+            while (reader.Read())
+            {
+                Teacher teacher = ReadTeacher(reader);
+                teachers.Add(teacher);
+            }
+            reader.Close();
+            dbConnection.Close();
+            return teachers;
+        }
+        public Teacher ReadTeacher(SqlDataReader reader)
+        {
+            int StudentID = (int)reader["CustomerID"];
+            string FirstName = (string)reader["FirstName"];
+            string LastName = (string)reader["LastName"];
+            string EmailAddress = (string)reader["EmailAddress"];
+            string PhoneNumber = (string)reader["PhoneNumber"];
+            int Role = (int)reader["Role"];
+            return new Teacher(StudentID, FirstName, LastName, EmailAddress, PhoneNumber, Role);
+        }
+
+    }
+}
