f6b1a8dc7fca3ced4801e3bca1e75eab0caf962e
SneakyPizza <5478583+SneakyPizza@users.noreply.github.com>
2020-03-10 14:24:33 +0100

    sql files added
    - Stored procedure for Students
    - Stored Procedure for Rooms
    - Create database sql

---
+++ Documentation/SQL/Create_Database.sql
@@ -0,0 +1,71 @@
+USE [ProjectDatabase]
+
+CREATE TABLE Person(
+    PersonID int NOT NULL PRIMARY KEY,
+    Firstname nvarchar(20) NOT NULL,
+    Lastname nvarchar(20) NOT NULL,
+    Email nvarchar(40) NOT NULL,
+    Phonenumber nvarchar(13) NOT NULL,
+    [Role] bit NOT NULL
+);
+
+CREATE TABLE [Order](
+    OrderID int NOT NULL PRIMARY KEY,
+    PersonID int NOT NULL FOREIGN KEY REFERENCES Person(PersonID),
+    Amount int NOT NULL,
+    --ProductID int NOT NULL FOREIGN KEY REFERENCES Product(ID),
+    [Timestamp] nvarchar(10) NOT NULL,
+);
+
+CREATE TABLE Room (
+    RoomID int NOT NULL PRIMARY KEY,
+    PersonID int NOT NULL FOREIGN KEY REFERENCES Person(PersonID),
+    RoomType nvarchar(20) NOT NULL,
+	RoomCapacity int NOT NULL
+);
+
+CREATE TABLE [Event](
+    EventID int NOT NULL PRIMARY KEY,
+    --ActivityID int NOT NULL FOREIGN KEY REFERENCES Activity(ID),
+    [Date] datetime NOT NULL,
+    [Description] nvarchar(30) NOT NULL,
+    SupervisorID int NOT NULL FOREIGN KEY REFERENCES Person(PersonID)
+    --EventJunctionID int NOT NULL FOREIGN KEY REFERENCES EventJunction(ID)
+);
+
+CREATE TABLE Sales(
+    salesID int NOT NULL PRIMARY KEY,
+    orderId int NOT NULL FOREIGN KEY REFERENCES [Order](OrderID),
+    Price money NOT NULL,
+    [Date] datetime NOT NULL,
+    InputDate datetime NOT NULL
+);
+
+CREATE TABLE Activity(
+    ActivityID int NOT NULL PRIMARY KEY,
+    [Name] nvarchar(30) NOT NULL,
+    [Date] datetime NOT NULL,
+    [Description] nvarchar(30) NOT NULL
+    );
+
+CREATE TABLE Product(
+    ProductID int NOT NULL PRIMARY KEY,
+    [Name] nvarchar(20) NOT NULL,
+    Price money NOT NULL,
+    BTWPercentile int NOT NULL,
+    Amount int NOT NULL,
+    [Description] nvarchar(30)
+);
+
+CREATE TABLE [EventJunction](
+    EventJunctionID int NOT NULL PRIMARY KEY,
+    PersonID int NOT NULL FOREIGN KEY REFERENCES Person(PersonID),
+    EventID int NOT NULL FOREIGN KEY REFERENCES [Event](EventID)
+);
+
+ALTER TABLE [Order]
+	ADD CONSTRAINT ProductID FOREIGN KEY (OrderID) REFERENCES Product(ProductID);
+
+ALTER TABLE [Event]
+	ADD CONSTRAINT ActivityID FOREIGN KEY (EventID) REFERENCES Activity(ActivityID),
+        CONSTRAINT EventJunctionID FOREIGN KEY (EventID) REFERENCES EventJunction(EventJunctionID);
---
+++ Documentation/SQL/GetPersonWhereRole.sql
@@ -0,0 +1,24 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Yornie Westink
+-- Create date: 10/03/2020
+-- Description: Get all Student information
+-- =============================================
+USE [ProjectDatabase];
+GO
+CREATE PROCEDURE dbo.GetAllPersonInfo
+	-- Add the parameters for the stored procedure here
+	@RoleId bit = 0
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT * FROM dbo.Person WHERE Person.Role = @RoleId
+END
+GO
