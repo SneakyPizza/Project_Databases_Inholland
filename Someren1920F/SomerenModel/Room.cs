29f046107872c063ec77249173b27b85a24e1eeb
larsdeg <54888533+larsdeg@users.noreply.github.com>
2020-03-11 11:24:57 +0100

    Update Room.cs

--- Someren1920F/SomerenModel/Room.cs
+++ Someren1920F/SomerenModel/Room.cs
@@ -8,14 +8,6 @@ namespace SomerenModel
 {
     public class Room
     {
-        public Room(int roomID, string roomType, int capacity)
-        {
-            RoomID = roomID;
-            RoomType = roomType;
-            Capacity = capacity;
-
-        }
-
         public int RoomID { get; set; } 
         public string RoomType { get; set; }
         public int Capacity { get; set; } // number of beds, either 4,6,8,12 or 16
