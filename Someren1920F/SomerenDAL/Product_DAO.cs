807747d9a15b5e0d6f8b58db0cd3df45064b3f24
larsdeg <54888533+larsdeg@users.noreply.github.com>
2020-03-20 12:17:03 +0100

    Week3A completely finished - LARS

---
+++ Someren1920F/SomerenDAL/Product_DAO.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+using System.Collections.ObjectModel;
+using SomerenModel;
+using System.Data.SqlTypes;
+
+namespace SomerenDAL
+{
+    public class Product_DAO : Base
+    {
+        public List<Product> Db_Get_All_Products()
+        {
+            return ReadTables(ExecuteSelectQuery("GetAllProducts"));
+        }
+
+        private List<Product> ReadTables(DataTable dataTable)
+        {
+            List<Product> products = new List<Product>();
+
+            foreach (DataRow dr in dataTable.Rows)
+            {
+                int id = (int)dr["ProductID"];
+                string name = (String)(dr["Name"].ToString());
+                decimal price = (decimal)(dr["Price"]);
+                int btw = (int)(dr["BTWPercentile"]);
+                int amount = (int)dr["Amount"];
+                string description = (String)(dr["Description"].ToString());
+                Product product = new Product(id, name, price, btw, amount, description);
+                products.Add(product);
+            }
+            return products;
+        }
+    }
+}
---
+++ Someren1920F/SomerenModel/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomerenModel
+{
+    public class Product
+    {
+        public Product(int id, string name, decimal price, int btw, int amount, string description)
+        {
+            ProductID = id;
+            Name = name;
+            Price = price;
+            BTWpercentile = btw;
+            Amount = amount;
+            Description = description;
+        }
+
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int BTWpercentile { get; set; }
+
+        public int Amount { get; set; }
+
+        public string Description { get; set; }
+
+    }
+}
