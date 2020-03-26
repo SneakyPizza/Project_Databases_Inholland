cd7eba4a573e7b86b752e9a396e7ad94232c3409
Yornie Westink <jinxie88@live.nl>
2020-03-26 10:59:45 +0100

    SQL insert activities

---
+++ Documentation/SQL/InsertActivities.sql
@@ -0,0 +1,10 @@
+USE [ProjectDatabase]
+INSERT [dbo].Activity([Activity].[Name], [Activity].[Date], Activity.[Description]) VALUES (N'Voetballen', '02/06/2020', N'Ballen met de voet.')
+INSERT [dbo].Activity([Activity].[Name], [Activity].[Date], Activity.[Description]) VALUES (N'Handballen', N'03/06/2020', N'Ballen met de hand')
+
+--INSERT [dbo].Activity([Activity].[Name], [Activity].[Date], Activity.[Description]) VALUES (N'Disco-zwemmen', N'04/06/2020 22:30:00', N'We vertellen niet wat je gaat doen, maar neem je zwemspullen mee.')
+INSERT [dbo].Activity([Activity].[Name], [Activity].[Date], Activity.[Description]) VALUES (N'Jagerbal', N'04/06/2020 08:30:20', N'Tikkertje met een bal.')
+INSERT [dbo].Activity([Activity].[Name], [Activity].[Date], Activity.[Description]) VALUES (N'Fietsenroof', N'06/06/2020 23:30:00', N'Oefenen voor later.')
+INSERT [dbo].Activity([Activity].[Name], [Activity].[Date], Activity.[Description]) VALUES (N'Lijntikkertje', N'03/06/2020 12:00:00', N'Tikkertje met een lijn.')
+INSERT [dbo].Activity([Activity].[Name], [Activity].[Date], Activity.[Description]) VALUES (N'Spijkerpoepen', N'05/06/2020 14:20:00', N'Schijten in een fles.')
+-- SET IDENTITY_INSERT [dbo].[Person] OFF
