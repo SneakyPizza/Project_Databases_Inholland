9b72da19b87b636d8f5b26ba044d984303a30e81
MisterKoen0202 <54888574+MisterKoen0202@users.noreply.github.com>
2020-03-27 15:20:13 +0100

    druk bezig

---
+++ Someren1920F/SomerenDAL/Supervisor_DAO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+using System.Collections.ObjectModel;
+using SomerenModel;
+using System.Data.SqlTypes;
+
+namespace SomerenDAL
+{
+    public class Supervisor_DAO
+    {
+        /*
+        public List<Supervisor> Db_Get_All_Supervisors()
+        {
+            SqlParameter sqlParameter = new SqlParameter("@RoleID", 0);
+            SqlParameter[] sqlp = new SqlParameter[] { sqlParameter };
+            return ReadTables(ExecuteSelectQuery("GetAllPersonInfo", sqlp));
+        }
+        */
+    }
+}
---
+++ Someren1920F/SomerenModel/Supervisor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomerenModel
+{
+    public class Supervisor
+    {
+        public Supervisor(int id, int TeacherId , int activityId)
+        {
+            EventJunctionID = id;
+            PersonId = TeacherId;
+            ActivityId = activityId;
+        }
+
+        public int EventJunctionID { get; set; }
+        public int PersonId { get; set; }
+        public int ActivityId { get; set; }
+    }
+}
